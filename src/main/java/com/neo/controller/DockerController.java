3c38255363da0bbb3a8dc8e47be367929c7f02ee
Jiahao <40542227+JiahaoLin2018@users.noreply.github.com>
2020-03-28 01:27:15 +0800

    Revert "优化代码"
    This reverts commit b6c3d46

--- src/main/java/com/neo/controller/DockerController.java
+++ src/main/java/com/neo/controller/DockerController.java
@@ -13,17 +13,16 @@ public class DockerController {
 
 
         public static void main(String[] args) {
-            System.out.println("第二次测试");
-            String str1 = "计算机" + "软件";
+
+            String str1 = new StringBuilder("计算机").append("软件").toString();
 
 // String str3= new StringBuilder("计算机软件").toString();
 
-            System.out.println(str1.equals(str1.intern()));
-            System.out.println(str1.equals(str1.intern()));
+            System.out.println(str1.intern() == str1);
 
-            String str2 = "Java(TM) SE sssssssssss";
+            String str2 = new StringBuilder("Java(TM) SE sssssssssss").toString();
 
-            System.out.println(str2.intern().equals(str2));
+            System.out.println(str2.intern() == str2);
 
         }
 
