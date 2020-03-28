52ba25ee5bd63e0036b88caa59041c5202c2f789
Jiahao <40542227+JiahaoLin2018@users.noreply.github.com>
2020-03-28 17:51:36 +0800

    Revert "优化代码"
    This reverts commit b6c3d46

--- src/main/java/com/neo/DockerApplication.java
+++ src/main/java/com/neo/DockerApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerApplication {
 
 	public static void main(String[] args) {
-System.out.println("sss");
-		
+
+		System.out.println("sssss");
+
 		SpringApplication.run(DockerApplication.class, args);
 	}
 }
